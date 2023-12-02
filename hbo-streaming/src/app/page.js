dafc5956741e373a44be65839502a07269463eea
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-12-02 01:57:03 -0500

    finished the page of the project will be working on create the user section soon

--- hbo-streaming/src/app/page.js
+++ hbo-streaming/src/app/page.js
@@ -15,7 +15,12 @@ export default function Home() {
         <div className="login-user__form">
           <div className="login-user__user-box">
             <img className="login-user__user-img" src="https://mighty.tools/mockmind-api/content/human/65.jpg"/>
+            <div className="login-user__user-name">Bryant</div>
           </div>
+        </div>
+        <div className="login-user__buttons">
+          <button className="login-user__adult">Add Adult</button>
+          <button className="login-user__adult">Add Kid</button>
         </div>
       </div>
     </div>
