0192aeac89a0cca5850e21718a1c1a46f0e49605
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-12-08 03:53:31 -0500

    fixed header logo image

--- hbo-streaming/src/app/components/layouts/MainLayout.js
+++ hbo-streaming/src/app/components/layouts/MainLayout.js
@@ -1,12 +1,15 @@
 import Header from '../UI/Header';
+import '../../styles/header.scss';
 
 const MainLayout = (props) => {
-    return (<>
+    return (<div style={{
+        background: 'linear-gradient(135deg, rgba(2,27,64,1) 11%, rgba(119,30,135,1) 100%)'
+    }}>
             <Header />
             <section className="content-container">
                 {props.children}
             </section>
-        </>
+        </div>
     )
 }
 
